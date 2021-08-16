bddbc1f2de1d9ae29d47b01f38e2dfb66c66192d
YusraSenimDede <50294199+YusraSenimDede@users.noreply.github.com>
2021-08-16 11:29:44 +0300

    project finish

--- app.js
+++ app.js
@@ -1,63 +1,84 @@
-window.onload=function() {
-new Vue ({
-        el : "#app",
+new Vue({
+        el: "#app",
         data: {
             player_heal: 100,
             monster_heal: 100,
             game_is_on: false,
+            logs: []
         },
         methods: {
-            start_game: function(){
-                this.game_is_on = true;    
+            start_game: function () {
+                this.game_is_on = true;
             },
-            attack: function(){
-                var point= Math.ceil(Math.random()*10);
-                this.monster_heal-=point;
-                this.monster_attack();                             
-                },
-            special_attack: function(){
-                var point= Math.ceil(Math.random()*25);
-                this.monster_heal-=point;
-                this.monster_attack();               
-            },
-            heal_up: function(){
-                var point= Math.ceil(Math.random()*20);
-                this.player_heal+=point;
+            attack: function () {
+                var point = Math.ceil(Math.random() * 10);
+                this.monster_heal -= point;
                 this.monster_attack();
-                
+                this.add_to_log({
+                    turn: "p",
+                    text: "OYUNCU ATAĞI (" + point + ")"
+                });
             },
-            give_up: function(){
-                this.player_heal=0;
-            
+            special_attack: function () {
+                var point = Math.ceil(Math.random() * 25);
+                this.monster_heal -= point;
+                this.monster_attack();
+                this.add_to_log({
+                    turn: "p",
+                    text: "ÖZEL OYUNCU ATAĞI (" + point + ")"
+                });
             },
-            monster_attack: function(){
-                var point= Math.ceil(Math.random()*15);
-                this.player_heal-=point;
-            
+            heal_up: function () {
+                var point = Math.ceil(Math.random() * 20);
+                this.player_heal += point;
+                this.monster_attack();
+                this.add_to_log({
+                    turn: "p",
+                    text: "İLK YARDIM (" + point + ")"
+                });
             },
+            give_up: function () {
+                this.player_heal = 0;
+                this.add_to_log({
+                    turn: "p",
+                    text: "OYUNCU PES ETTİ"
+                });
+            },
+            monster_attack: function () {
+                var point = Math.ceil(Math.random() * 15);
+                this.player_heal -= point;
+                this.add_to_log({
+                    turn: "m",
+                    text: "CANAVAR ATAĞI (" + point + ")"
+                });
+            },
+            add_to_log: function (log) {
+                this.logs.push(log);
+            }
         },
-        watch :{
-            player_heal: function(value){
-                if(value<=0){
-                    this.player_heal=0;
-                    if(confirm("Oyunu Kaybettin. Tekrar denemek ister misin ? ")){
-                        this.player_heal= 100;
-                        this.monster_heal= 100;
+        watch: {
+            player_heal: function (value) {
+                if (value <= 0) {
+                    this.player_heal = 0;
+                    if (confirm("Oyunu Kaybettin. Tekrar denemek ister misin ? ")) {
+                        this.player_heal = 100;
+                        this.monster_heal = 100;
+                        this.logs=[];
                     }
-                }else if(value>=100){
-                    this.player_heal=100;
+                } else if (value >= 100) {
+                    this.player_heal = 100;
                 }
             },
-            monster_heal: function(value){
-                if(value<=0){
-                    this.monster_heal=0;
-                    if(confirm("Oyunu Kazandın. Tekrar denemek ister misin ? ")){
-                        this.player_heal= 100;
-                        this.monster_heal= 100;
-                        
+            monster_heal: function (value) {
+                if (value <= 0) {
+                    this.monster_heal = 0;
+                    if (confirm("Oyunu Kazandın. Tekrar denemek ister misin ? ")) {
+                        this.player_heal = 100;
+                        this.monster_heal = 100;
+                        this.logs=[];
+
                     }
                 }
             }
         }
     });
-};
